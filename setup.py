6185cb7d7493340b266665699f45091a36c5c1e2
Jaepil Jeong <jaepil@appspand.com>
2014-12-21 18:42:39 +0900

    Download jar files during the installation.

--- setup.py
+++ setup.py
@@ -2,16 +2,46 @@
 #
 # Copyright (c) 2014 Jaepil Jeong
 
+from __future__ import print_function
+
+import os
+
 from distutils.core import setup
 
+try:
+    from urllib import urlretrieve
+except:
+    from urllib.request import urlretrieve
 
-__version__ = "0.1.0"
+
+__VERSION__ = "0.1.0"
+
+_JAVA_LIB_URLS = [
+    "https://repo1.maven.org/maven2/org/scala-lang/scala-library/2.11.4/scala-library-2.11.4.jar",
+    "https://repo1.maven.org/maven2/org/apache/thrift/libthrift/0.9.2/libthrift-0.9.2.jar",
+    "https://repo1.maven.org/maven2/org/yaml/snakeyaml/1.14/snakeyaml-1.14.jar",
+    "https://repo1.maven.org/maven2/com/twitter/twitter-text/1.10.2/twitter-text-1.10.2.jar",
+    "https://repo1.maven.org/maven2/com/twitter/penguin/korean-text/2.3.3/korean-text-2.3.3.jar",
+]
+_PATH_TO_LIB = os.path.join(os.path.abspath(os.path.dirname((__file__))), "twkorean/data/lib")
+
+
+def download_jars(target_path):
+    for url in _JAVA_LIB_URLS:
+        jar_name = os.path.basename(url)
+        jar_path = os.path.join(target_path, jar_name)
+        if not os.path.exists(jar_path):
+            print("Downloading java package:", jar_name)
+            urlretrieve(url, jar_path)
+
+
+download_jars(target_path=_PATH_TO_LIB)
 
 
 setup(
     name="twkorean",
     license="Apache 2.0",
-    version=__version__,
+    version=__VERSION__,
     packages=["twkorean"],
     package_dir={"twkorean": "twkorean"},
     package_data={
